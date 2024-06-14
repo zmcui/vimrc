b3fb4b6f1300a324db0b9520397cb5e4d598836b
zmc <zongmincui@asrmicro.com>
2024-06-14 11:14:35 +0800

    OverView: refine setup script

--- setup.sh
+++ setup.sh
@@ -11,10 +11,14 @@ function linkPath() {
         unlink $1
     elif [ -e $1 ] ; then # file
         echo "file: $(realpath `$1`)"
-        mv -rf $1 $1+"_bp"
+        mv $1 $1"_bp"
     fi
     ln -s $2 $1
-    echo "$1 -> $2 done!"
+    if [[ $? -eq 0 ]]; then
+        echo "$1 -> $2 done!"
+    else
+        echo "$1 -> $2 fail!"
+    fi
 }
 
 linkPath ~/.vimrc `pwd`/vimrc
